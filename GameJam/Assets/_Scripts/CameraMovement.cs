0125360f0ff301124a698ed38f04da70cf784d7a
Burritosss <vdkuipb@gmail.com>
2016-04-11 13:57:35 +0200

    added: timerunning ui en outofbounds destroy

--- GameJam/Assets/_Scripts/CameraMovement.cs
+++ GameJam/Assets/_Scripts/CameraMovement.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
-	float currentTime;
-	float speed;
-	void update(){
+	public Text timeRunning;
+	float currentTime = 0;
+	float speed = 0.005f;
+	void Update(){
 		currentTime += Time.deltaTime;
-		if (currentTime == 10f) {
-			speed = 0.1f;
+		if (currentTime >= 10f) {
+			speed = 0.01f;
 		}
-		if (currentTime == 20f) {
-			speed = 0.15f;
+		if (currentTime >= 20f) {
+			speed = 0.015f;
 		}
 		transform.Translate (Vector2.up * speed);
+		timeRunning.text = "Time running: " + Mathf.Floor(currentTime) + " Seconds";
 	}
 }
