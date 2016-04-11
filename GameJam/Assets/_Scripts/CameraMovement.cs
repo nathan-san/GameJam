a8384616b609bba981f2363a95a578461390cbf2
Burritosss <vdkuipb@gmail.com>
2016-04-11 13:41:53 +0200

    added Camera Movement

---
+++ GameJam/Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	float currentTime;
+	float speed;
+	void update(){
+		currentTime += Time.deltaTime;
+		if (currentTime == 10f) {
+			speed = 0.1f;
+		}
+		if (currentTime == 20f) {
+			speed = 0.15f;
+		}
+		transform.Translate (Vector2.up * speed);
+	}
+}
